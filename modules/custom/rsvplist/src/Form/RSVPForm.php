118de9e9bbe1cadae45d9495b24ca3d7b364e883
Wasim Hyder <wasim@creativefaze.com>
2022-07-12 16:37:36 +0500

    Created info file and form for RSVP list

---
+++ modules/custom/rsvplist/src/Form/RSVPForm.php
@@ -0,0 +1,52 @@
+<?php
+/**
+  * @file
+  * Contains \Drupal\rsvplist\Form\RSVPForm.
+  */
+namespace Drupal\rsvplist\From;
+
+use Drupal\Core\Database\Database;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Provides an RSVP Email form.
+ */
+class RSVPForm extends FormBase {
+  /**
+   * (@inheritdoc)
+   */
+  public function getFormId() {
+    return 'rsvplist_email_form';
+  }
+
+  /**
+   * (@inheritdoc)
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $node = \Drupal::routeMatch()->getParameter('node');
+    $nid = $node->nid->value;
+    $form['email'] = array(
+      '#title' => t('Email Address'),
+      '#type' => 'textfield',
+      '#size' => 25,
+      '#description' => t('We will send you the updates to Email Address you provide.'),
+      '#required' => TRUE,
+    );
+    $form['submit'] = array(
+      '#type' => 'submit',
+      '#value' => t('RSVP'),
+    );
+    $form['nid'] = array(
+      '#type' => 'hidden',
+      '#value' => $nid,
+    );
+    return $form;
+  }
+  /**
+   * (@inheritdoc)
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    drupal_set_message(t('The form is working.'));
+  }
+}
