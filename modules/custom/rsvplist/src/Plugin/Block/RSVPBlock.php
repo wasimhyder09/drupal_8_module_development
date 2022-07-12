816123d8b40e0590457411829e44ae4c8c488434
Wasim Hyder <wasim@creativefaze.com>
2022-07-12 22:31:40 +0500

    Display form in block and set permissions

--- modules/custom/rsvplist/src/Plugin/Block/RSVPBlock.php
+++ modules/custom/rsvplist/src/Plugin/Block/RSVPBlock.php
@@ -23,6 +23,16 @@ class RSVPBlock extends BlockBase {
    */
 
   public function build() {
-    return array('#markup' => $this->t('My RSVP List Block'));
+    return \Drupal::formBuilder()->getForm('Drupal\rsvplist\Form\rSVPForm');
+  }
+
+  public function blockAccess(AccountInterface $account) {
+    /** @var \Drupal\node\Entity\Node $node */
+    $node = \Drupal::routeMatch()->getParameter('node');
+    $nid = $node->nid->value;
+    if(is_numeric($nid)) {
+      return AccessResult::allowedIfHasPermission($account, 'view rsvplist');
+    }
+    return AccessResult::forbidden();
   }
 }
