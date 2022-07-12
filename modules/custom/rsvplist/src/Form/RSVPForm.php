1fd77e0072f93be31fa9584cd08475d7adc7539e
Wasim Hyder <wasim@creativefaze.com>
2022-07-12 17:02:56 +0500

    Created form validation method

--- modules/custom/rsvplist/src/Form/RSVPForm.php
+++ modules/custom/rsvplist/src/Form/RSVPForm.php
@@ -43,6 +43,17 @@ class RSVPForm extends FormBase {
     );
     return $form;
   }
+
+  /**
+   * (@inheritdoc)
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    $value = $form_state->getValue('email');
+    if($value == !\Drupal::service('email.validator')->isValid($value)) {
+      $form_state->setErrorByName('email', t('Email address %mail is not valid.', array('%mail' => $value)));
+    }
+  }
+
   /**
    * (@inheritdoc)
    */
